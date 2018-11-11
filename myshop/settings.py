9e50fc0ef4ae1e9a4a3e5f16728c69213a5ea1a3
qwertqqq <39577660+qwertqqq@users.noreply.github.com>
2018-11-11 20:38:08 +0800

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w#c2p$9qpf@+%_f14c9g1%4lm*o0s2ht^*+4mx^77l($)l!1!6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.nanhong521.com']
 
 
 # Application definition
